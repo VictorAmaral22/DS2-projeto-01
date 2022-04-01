97e9b3d5c9c50895dffafd1376f230db1365409d
Victor Amaral <victortavamaral@gmail.com>
2022-04-01 15:13:01 -0300

    Add files via upload

--- src/controllers/games-controller.js
+++ src/controllers/games-controller.js
@@ -1,14 +1,21 @@
-const {games, users} = require('../database/db');
+const {games, users, generos} = require('../database/db');
 const { nanoid } = require('nanoid');
 
 class GamesController {
     async listaGeral (req, res) {
         const user = req.session.user;
-        return res.render('listagem/listagem', { games: games, user: user });
+        return res.render('listagem', { games: games, user: user });
     }
-    async detalhar (req, res) {}
-    async addLista (req, res) {}
-    async detalhar (req, res) {}
+    async detalhar (req, res) {
+        const user = req.session.user;
+        const { id } = req.params;
+        const game = games.filter(item => item.id == id);
+        return res.render('detalhar', { game: game[0], generos: generos, user: user });
+    }
+    async addLista (req, res) {
+        const user = req.session.user;
+        const { id } = req.params;
+    }
     async alterar (req, res) {}
     async deletar (req, res) {}    
     async add (req, res) {}
--- src/routes/user-routes.js
+++ src/routes/user-routes.js
@@ -1,4 +1,6 @@
-const { Router } = require('express');
+const {
+    Router
+} = require('express');
 
 const UsersController = require('../controllers/user-controller');
 
@@ -10,10 +12,11 @@ routes.post('/cadastrar', userController.cadastrar);
 routes.post('/login', userController.login);
 
 
-routes.get('/getUser',(req,res,next)=>{
-    if(req.session.user) next();
+routes.get('/getUser', (req, res, next) => {
+    if (req.session.user) next();
     else res.redirect('/login.html');
 }, userController.getUser);
+
 routes.post('/editUser', userController.editUser);
 routes.post('/delUser', userController.delUser);
 
