9b0c1de893bfccb17eae337b55130b15a6d68e41
victor-grandmasterwitcher <victortavamaral@gmail.com>
2022-04-02 13:04:02 -0300

    vamo

--- src/controllers/games-controller.js
+++ src/controllers/games-controller.js
@@ -4,28 +4,54 @@ const { nanoid } = require('nanoid');
 class GamesController {
     async listaGeral (req, res) {
         const user = req.session.user;
-        return res.render('listagem', { games: games, user: user });
+        return res.render('listagem/listagem', { games: games, user: user });
     }
+
     async add (req, res) {
         const { id, nome, dev, datalanc, sinopse, genero, capa, wallpaper } = req.body;
         const game = { id: id, nome: nome, dev: dev, datalanc: datalanc, sinopse: sinopse, genero: genero, capa: capa, wallpaper: wallpaper };
         games.push(game);
-        return res.redirect('listagem');
+        return res.redirect('listagem/listagem');
     }
+
     async detalhar (req, res) {
         const user = req.session.user;
         const { id } = req.params;
         const game = games.filter(item => item.id == id);
-        return res.render('detalhar', { game: game[0], generos: generos, user: user });
+        return res.render('detalhar/detalhar', { game: game[0], generos: generos, user: user });
     }
+
     async addLista (req, res) {
         const user = req.session.user;
         const { id } = req.params;
         const userList = users.filter(item => item.id == user.id)[0].favoritos;
         userList.push(id);
     }
-    async alterar (req, res) {}
-    async deletar (req, res) {}    
+
+    async renderAlterar (req, res) {
+        const { id } = req.params;
+        return res.render('alterar/alterar');
+    }
+
+    async alterar (req, res) {
+        const { id } = req.params;
+        const { nome, dev, datalanc, sinopse, genero, capa, wallpaper } = req.body;
+        const game = { id: id, nome: nome, dev: dev, datalanc: datalanc, sinopse: sinopse, genero: genero, capa: capa, wallpaper: wallpaper };
+
+        games.map(item => {
+            if(item.id == id) item = game;
+        })
+
+        return res.redirect('/games');
+    }
+
+    async renderDeletar (req, res) {
+        const { id } = req.params;
+    }
+
+    async deletar (req, res) {
+        const { id } = req.params;
+    }
 }
 
 module.exports = { GamesController }
--- src/routes/games-routes.js
+++ src/routes/games-routes.js
@@ -3,16 +3,30 @@ const { Router } = require('express');
 const { GamesController } = require('../controllers/games-controller');
 
 const routes = Router();
+
 const gamesController = new GamesController();
+
+const middlewareAdmin = (req, res, next) => {
+    const user = req.session.user;
+    console.log(user)
+    if(user && user.tipo == 1){
+        next();
+    } else {
+       return res.redirect('/games');
+    }
+};
 
 routes.get('/', gamesController.listaGeral);
 
 routes.post('/add', gamesController.add);
 
 routes.get('/:id', gamesController.detalhar);
-routes.get('/:id/deletar', gamesController.deletar);
-routes.get('/:id/alterar', gamesController.alterar);
 routes.get('/:id/add-lista', gamesController.addLista);
+
+routes.get('/:id/deletar', middlewareAdmin, gamesController.renderDeletar);
+routes.post('/:id/deletar', middlewareAdmin, gamesController.deletar);
+routes.get('/:id/alterar', middlewareAdmin, gamesController.renderAlterar);
+routes.post('/:id/alterar', middlewareAdmin, gamesController.alterar);
 
 
 module.exports = routes;
