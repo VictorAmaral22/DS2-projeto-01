44f71acc9564740d1f73c23dfdf77f5215368036
victor-grandmasterwitcher <victortavamaral@gmail.com>
2022-04-07 18:13:22 -0300

    testandooo

--- src/index.js
+++ src/index.js
@@ -46,4 +46,6 @@ app.use('*', (req, res) => {
     `);
 })
 
-app.listen(3000, () => console.log('Server iniciado em http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server iniciado em ${PORT}`));
